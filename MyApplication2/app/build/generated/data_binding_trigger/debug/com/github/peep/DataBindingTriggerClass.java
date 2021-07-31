1f9c77a2d1a0506b146f9dd8afc8a47cd7a68b3c
kjsong99 <mine7118@kpu.ac.kr>
2021-08-01 02:13:13 +0900

    Apply .gitignore

--- MyApplication2/app/build/generated/data_binding_trigger/debug/com/github/peep/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.github.peep;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- MyApplication2/app/build/generated/source/kapt/debug/com/github/peep/BR.java
+++
@@ -1,5 +0,0 @@
-package com.github.peep;
-
-public class BR {
-  public static final int _all = 0;
-}

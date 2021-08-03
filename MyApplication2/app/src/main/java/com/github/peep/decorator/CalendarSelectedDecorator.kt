d8755a25d1bea553ad7352f3a3c99bb74545e363
hhhminme <kenma409@gmail.com>
2021-08-03 15:09:44 +0900

    calender class 추가

---
+++ MyApplication2/app/src/main/java/com/github/peep/decorator/CalendarSelectedDecorator.kt
@@ -0,0 +1,40 @@
+package com.catlove.gitcat
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.text.style.ForegroundColorSpan
+import android.util.Log
+import com.github.peep.R
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
+
+class CalendarSelectedDecorator @SuppressLint("UseCompatLoadingForDrawables") constructor(
+    dates: CalendarDay,
+    context: Activity
+) : DayViewDecorator {
+    private var drawable: Drawable
+    private var dates: CalendarDay? = dates
+
+    init {
+        drawable = context.getDrawable(R.drawable.calendar_select)!!
+    }
+
+    override fun shouldDecorate(day: CalendarDay?): Boolean {
+        return dates != null && day!!.equals(dates)
+    }
+
+    override fun decorate(view: DayViewFacade?) {
+        //view!!.setSelectionDrawable(drawable)
+        Log.d("decorate", "decorate:  ")
+        view!!.setBackgroundDrawable(drawable)
+        view.addSpan(ForegroundColorSpan(Color.BLACK))
+        //view!!.addSpan(DotSpan(5f, color))
+    }
+
+
+
+
+}

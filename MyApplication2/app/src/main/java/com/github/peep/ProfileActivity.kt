22e1378b15923f7597ac300474e0221b9d15bf84
hhhminme <kenma409@gmail.com>
2021-09-12 16:31:11 +0900

    3차 프로토타입 머지

--- MyApplication2/app/src/main/java/com/github/peep/ProfileActivity.kt
+++ MyApplication2/app/src/main/java/com/github/peep/ProfileActivity.kt
@@ -12,10 +12,8 @@ import android.view.LayoutInflater
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
-import com.bumptech.glide.Glide
 import com.github.peep.DB.UserDB
 import com.github.peep.databinding.ActivityProfileBinding
-import com.github.peep.decorator.AlertDesign
 import com.peep.githubapitest.githubpapi.ApiClient
 import com.peep.githubapitest.githubpapi.GithubInterface
 import com.peep.githubapitest.model.User
@@ -29,6 +27,7 @@ import retrofit2.Response
 
 class ProfileActivity : AppCompatActivity() {
     lateinit var mBinding : ActivityProfileBinding
+    private var userDb : UserDB? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_profile)
@@ -41,34 +40,36 @@ class ProfileActivity : AppCompatActivity() {
             finish()
         }
         mBinding.backButton.setOnClickListener {
-            showSettingPopup("로그아웃 하시겠습니까?")
+            showSettingPopup()
         }
     }
-    fun showSettingPopup(string : String){
-        AlertDesign(this)
-            .setTitle("권한 변경")
-            .setMessage(string)
-            .setPositiveButton("예") {
+    fun showSettingPopup(){
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.alert_popup,null)
+        var textView = view.findViewById<TextView>(R.id.alert_textview)
+        textView.text = "권한을 변경하시겠습니까?\n변경 시, 재로그인이 필요합니다."
+        val alertDialog = AlertDialog.Builder(this)
+            .setTitle("권한 설정 변경")
+            .setPositiveButton("확인"){ dialog, which ->
                 logout()
                 var intent=Intent(this,HomeActivity::class.java)
-                finishAffinity()
+                finish()
                 startActivity(intent)
             }
-            .setNegativeButton("취소"){
-                finish()
-            }
+            .setNegativeButton("취소",null)
+            .create()
 
-            .show()
+        alertDialog.setView(view)
+        alertDialog.show()
     }
     fun getUser(){
         var GithubService= ApiClient.client.create(GithubInterface::class.java)
         val call=GithubService.getUser()
         call.enqueue(object: Callback<User> {
             override fun onResponse(call: Call<User>, response: Response<User>) {
-                Log.d("fullresponse", response.toString())
                 if (response.code() == 200) {
                     val user=response.body()
-                    Glide.with(this@ProfileActivity).load(user?.avatar_url).circleCrop().into(mBinding.gitProfileIv)
+                    Picasso.get().load(user?.avatar_url).into(mBinding.gitProfileIv)
                     mBinding.gitUsernameTv.text = user?.login
                 } else {
                     Log.e("err",response.code().toString())
@@ -87,10 +88,5 @@ class ProfileActivity : AppCompatActivity() {
     override fun onDestroy() {
         UserDB.destroyInstance()
         super.onDestroy()
-    }
-
-    override fun onBackPressed() {
-        //super.onBackPressed()
-        Toast.makeText(this,"시작하기 버튼을 눌러주세요.",Toast.LENGTH_SHORT).show()
     }
 }
